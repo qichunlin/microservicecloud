52edb4b11782f799fe9c90897ab72c643a3ede57
qichunlin <737795279@qq.com>
2019-07-25 02:47:27 +0800

    [UPDATE] 完成微服务的消费者和提供者的测试

---
+++ microservicecloud-provider-dept-8001/src/main/java/com/legend/springcloud/DeptProvider8001_App.java
@@ -0,0 +1,13 @@
+package com.legend.springcloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptProvider8001_App {
+    public static void main(String[] args){
+        SpringApplication.run(DeptProvider8001_App.class,args);
+
+    }
+}
