6fe76f82bda10354778d60870acf4590b860de8f
qichunlin <737795279@qq.com>
2019-08-01 23:50:25 +0800

    [ADD] 创建Hystrix-dashboard 监控信息

---
+++ microservicecloud-consumer-hystrix-dashboard/src/main/java/com/legend/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,15 @@
+package com.legend.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@SpringBootApplication
+@EnableHystrixDashboard  //开启 HystrixDashboard仪表盘监控注解
+public class DeptConsumer_DashBoard_App
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
+	}
+}
